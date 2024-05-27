175b41d93ecba38bd61faee5fb1e73ad6027f03f
dcarone5311 <32916309+dcarone5311@users.noreply.github.com>
2024-05-27 08:59:05 -0700

    Chapter 6
    Gallery: Building an AR App

---
+++ Assets/Scripts/ImageButtons.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageButtons : MonoBehaviour
+{
+    [SerializeField] GameObject imageButtonPrefab;
+    [SerializeField] ImagesData imagesData;
+    [SerializeField] AddPictureMode addPicture;
+
+    private void Start()
+    {
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            GameObject.Destroy(transform.GetChild(i).gameObject);
+        }
+        foreach(ImageInfo image in imagesData.images)
+        {
+            GameObject obj = Instantiate(imageButtonPrefab, transform);
+            RawImage rawimage = obj.GetComponent<RawImage>();
+            rawimage.texture = image.texture;
+            Button button = obj.GetComponent <Button>();
+            button.onClick.AddListener(() => OnClick(image));
+        }
+
+    }
+
+    private void OnClick(ImageInfo image)
+    {
+        addPicture.imageInfo = image;
+        InteractionController.EnableMode("AddPicture");
+    }
+}
---
+++ Assets/Scripts/SelectImageMode.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectImageMode : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        {
+            UIController.ShowUI("SelectImage");
+        }
+    }
+
+}
